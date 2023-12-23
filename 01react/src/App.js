2ea74bb7bb47aa26349c3d5b77ab66bbe58217d9
Ajay Purbiya <Ajaypupurbiya@gmail.com>
2023-12-23 13:00:31 +0530

    this rc

---
+++ 01react/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './chai'
+function App() {
+  return (
+    <>
+    <Chai/>
+    {/* <h1>chai aur react | Ajay Purbiya <br/>
+      In this video I learn how to create react project.
+    </h1> */}
+    </>
+  );
+}
+
+export default App;
